dd264997321f22fa01998ff43d6668c886c9fb0d
lum1narie <sn.lum.fallenrose.aol@gmail.com>
2021-05-30 15:51:05 +0900

    Axiosページを追加

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import Home from "./pages/Home.vue";
 import NotFound from "./pages/NotFound.vue";
+import Axios from "./pages/Axios.vue";
 
 export const routes = [
   { path: "/", component: Home },
+  { path: "/axios", component: Axios },
   { path: "/:path(.*)", component: NotFound },
 ];
