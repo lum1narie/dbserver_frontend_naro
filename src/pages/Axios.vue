87aac62e1ec44b63ab85eeadecedd260d9226d44
lum1narie <sn.lum.fallenrose.aol@gmail.com>
2021-05-30 16:38:19 +0900

    pipedreamに送信するテストを実装した

--- src/pages/Axios.vue
+++ src/pages/Axios.vue
@@ -8,7 +8,32 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "Axios",
+  setup() {
+    const url = "https://engufr6lewbpr28.m.pipedream.net";
+    const sendGet = () => axios.get(url);
+    const sendPost = () =>
+      axios.post(url, {
+        traQID: "lum1narie",
+        grade: 20,
+      });
+    const sendPut = () =>
+      axios.put(url, {
+        testData: { string: "string", number: 1 },
+      });
+    const sendDelete = () =>
+      axios.delete(url, {
+        data: { po: 1 },
+      });
+    return {
+      sendGet,
+      sendPost,
+      sendPut,
+      sendDelete,
+    };
+  },
 };
 </script>
